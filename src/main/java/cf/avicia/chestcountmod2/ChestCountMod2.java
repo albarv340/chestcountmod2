a442f679818d29007acf684a5304e7dc3a59dbb5
albarv340 <54350887+albarv340@users.noreply.github.com>
2023-02-26 09:47:29 +0100

    initial commit

---
+++ src/main/java/cf/avicia/chestcountmod2/ChestCountMod2.java
@@ -0,0 +1,10 @@
+package cf.avicia.chestcountmod2;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ChestCountMod2 implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/cf/avicia/chestcountmod2/client/ChestCountMod2Client.java
@@ -0,0 +1,13 @@
+package cf.avicia.chestcountmod2.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ChestCountMod2Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
